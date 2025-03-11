4fcb9d7517df2fd732e1a509ce80a0a473cf5148
deerwho <aleksandar.jelenkovic22@gmail.com>
2025-03-11 15:27:53 +0100

    Update fligh.service.ts

--- src/services/fligh.service.ts
+++ src/services/fligh.service.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 const client = axios.create({
-        baseURL: process.env['FLIGHT_API_BASE_URL'] || 'https://flight.pequla.com/api',
-        headers: {
-            'Accept': 'application/json',
-            'X-Client-Name': 'KVA/2025',
-        },
-        validateStatus: (status: number) => {
-            return status === 200 // Resolve only if the status code is 200
-        }
-    });
+    baseURL: 'https://flight.pequla.com/api',
+    headers: {
+        'Accept': 'application/json',
+        'X-Client-Name': 'KVA/2025',
+    },
+    validateStatus: (status: number) => {
+        return status === 200 // Resolve only if the status code is 200
+    }
+});
 
 export class FlightService {
     static async getFlight(page: number = 0, size: number = 10) {
