89a0c084c9f7f8d76d1b83de9e48f2c8794dc852
deerwho <aleksandar.jelenkovic22@gmail.com>
2025-03-15 13:34:17 +0100

    3.3.25

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,13 +5,29 @@ import { AxiosError } from 'axios';
 import { MovieModel } from '../../model/movie.model';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, MatButtonModule, MatCardModule, ],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, FormsModule,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  public email: string = ''
+  public password: string = ''
+
+  public updateEmail(e: any) {
+    this.email = e.target.value
+  }
+  public updatePassword(e: any) {
+    this.password = e.target.value
+}
+  
+    public doLogin() {
+      alert(`${this.email} ${this.password}`)
+}
 }
